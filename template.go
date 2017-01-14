43b22607a7c5372a456cb279fa6d17d2a19a35ca
Akira Chiku <akira.chiku@gmail.com>
2017-01-14 17:12:17 +0900

    Update template

--- template.go
+++ template.go
@@ -4,9 +4,33 @@ import (
 	"bytes"
 	"encoding/json"
 	"html/template"
+	"log"
+	"os"
+	"reflect"
+	"strings"
 )
 
 type dict map[string]interface{}
+
+var funcMap = template.FuncMap{
+	"title": strings.Title,
+	"default": func(arg, value interface{}) interface{} {
+		v := reflect.ValueOf(value)
+		switch v.Kind() {
+		case reflect.String, reflect.Slice, reflect.Array, reflect.Map:
+			if v.Len() == 0 {
+				return arg
+			}
+		case reflect.Bool:
+			if !v.Bool() {
+				return arg
+			}
+		default:
+			return value
+		}
+		return value
+	},
+}
 
 func newDict(data []byte) (dict, error) {
 	var d dict
@@ -26,7 +50,7 @@ func (d *dict) toJSON() (string, error) {
 
 func fill(msg string, d dict) (string, error) {
 	b := &bytes.Buffer{}
-	t, err := template.New("").Parse(msg)
+	t, err := template.New("").Funcs(funcMap).Parse(msg)
 	if err != nil {
 		return "", err
 	}
@@ -34,4 +58,37 @@ func fill(msg string, d dict) (string, error) {
 		return "", err
 	}
 	return b.String(), nil
+}
+
+func te() {
+	// First we create a FuncMap with which to register the function.
+	funcMap := template.FuncMap{
+		// The name "title" is what the function will be called in the template text.
+		"title": strings.Title,
+	}
+
+	// A simple template definition to test our function.
+	// We print the input text several ways:
+	// - the original
+	// - title-cased
+	// - title-cased and then printed with %q
+	// - printed with %q and then title-cased.
+	const templateText = `
+Input: {{printf "%q" .}}
+Output 0: {{title .}}
+Output 1: {{title . | printf "%q"}}
+Output 2: {{printf "%q" . | title}}
+`
+
+	// Create a template, add the function map, and parse the text.
+	tmpl, err := template.New("titleTest").Funcs(funcMap).Parse(templateText)
+	if err != nil {
+		log.Fatalf("parsing: %s", err)
+	}
+
+	// Run the template to verify the output.
+	err = tmpl.Execute(os.Stdout, "the go programming language")
+	if err != nil {
+		log.Fatalf("execution: %s", err)
+	}
 }
--- template_test.go
+++ template_test.go
@@ -10,7 +10,21 @@ func TestFillTemplate(t *testing.T) {
 		"key4":   "日本語",
 		"key100": "無い",
 	}
-	msg := "{{.key1}} and {{.key2}}, {{.key3}}, {{.key4}}, {{.noKey}}"
+	msg := `{{.key1}} and {{.key2}}, {{.key3}}, {{.key4}}, {{.noKey}} {{default .noKey "val"}}`
+	s, err := fill(msg, d)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf(s)
+}
+
+func TestFillTemplateFuncDefault(t *testing.T) {
+	d := dict{
+		"key1": "this is the first key",
+		"key2": "this is the second key",
+		"key3": "",
+	}
+	msg := `{{"output" | title }} and {{.key2 | title }}, and {{ .key3 | default "default" }}`
 	s, err := fill(msg, d)
 	if err != nil {
 		t.Fatal(err)
@@ -40,4 +54,8 @@ func TestNewDict(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Logf("%+v", d)
+}
+
+func TestTe(t *testing.T) {
+	te()
 }
